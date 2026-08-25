014e7a43e5ca1afd165bab47588129f3dbbfdbbb
Roshan <roshansharma9379@gmail.com>
2026-08-25 15:20:51 +0530

    Made the design response for mobile devices

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../context/AppContext";
 import { assets, JobCategories, JobLocations } from "../assets/assets";
 import JobCard from "./JobCard";
@@ -6,6 +6,7 @@ import JobCard from "./JobCard";
 const JobListing = () => {
     const {isSearched, searchFilter, setSearchFilter, jobs} = useContext(AppContext);
 
+    const [showFilter, setShowFilter] = useState(false);
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       <div className="w-full lg:w-1/4 bg-white px-4">
@@ -31,7 +32,11 @@ const JobListing = () => {
             )
         }
 
-        <div className="max-lg:hidden">
+        <button onClick={e => setShowFilter(prev => !prev)} className="px-6 py-1.5 rounded border border-gray-400 lg:hodden">
+            {showFilter ? "Close" : "Filters"}
+        </button>
+
+        <div className={showFilter ? "" : "max-lg:hidden"}>
             <h4 className="font-medium text-lg py-4">Search by Category</h4>
             <ul className="space-y-4 text-gray-600">
                 {
@@ -45,7 +50,7 @@ const JobListing = () => {
             </ul>
         </div>
 
-        <div className="max-lg:hidden">
+        <div className={showFilter ? "" : "max-lg:hidden"}>
             <h4 className="font-medium text-lg py-4 pt-14">Search by Location</h4>
             <ul className="space-y-4 text-gray-600">
                 {
