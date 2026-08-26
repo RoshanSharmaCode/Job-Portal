b7a341be4a743563598e411b6b8d3bdeffe1a360
Roshan <roshansharma9379@gmail.com>
2026-08-26 15:31:01 +0530

    Created Compnay details page

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const JobCard = ({job}) => {
+const JobCard = ({ job }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="border p-6 shadow rounded">
       <div className="flex justify-between items-center">
@@ -9,16 +12,39 @@ const JobCard = ({job}) => {
       </div>
       <h4 className="font-medium text-xl mt-2">{job.title}</h4>
       <div className="flex items-center gap-3 mt-2 text-xs">
-        <span className="bg-blue-50 border border-blue-200 px-4 py-1.5 rounded">{job.location}</span>
-        <span className="bg-red-50 border border-red-200 px-4 py-1.5 rounded">{job.level}</span>
+        <span className="bg-blue-50 border border-blue-200 px-4 py-1.5 rounded">
+          {job.location}
+        </span>
+        <span className="bg-red-50 border border-red-200 px-4 py-1.5 rounded">
+          {job.level}
+        </span>
       </div>
-      <p className="text-gray-500 text-sm mt-4" dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }} />
+      <p
+        className="text-gray-500 text-sm mt-4"
+        dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}
+      />
       <div className="mt-4 flex gap-4 text-sm">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">Apply now</button>
-        <button className="text-gray-500 border border-gray-500 rounded px-4 py-2">Learn more</button>
+        <button
+          onClick={() => {
+            navigate(`/apply-job/${job._id}`);
+            scrollTo(0, 0);
+          }}
+          className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
+        >
+          Apply now
+        </button>
+        <button
+          onClick={() => {
+            navigate(`/apply-job/${job._id}`);
+            scrollTo(0, 0);
+          }}
+          className="text-gray-500 border border-gray-500 rounded px-4 py-2 cursor-pointer"
+        >
+          Learn more
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default JobCard
+export default JobCard;
