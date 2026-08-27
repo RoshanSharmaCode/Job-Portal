d8b73801af45881eeb70be9855ffe5a403b00412
Roshan <roshansharma9379@gmail.com>
2026-08-27 20:04:49 +0530

    Completed View Applications page

--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
+import React from "react";
+import { assets, viewApplicationsPageData } from "../assets/assets";
 
 const ViewApplications = () => {
   return (
-    <div>
-      View applications
-    </div>
-  )
-}
+    <div className="container mx-auto p-2">
+      <div>
+        <table className="w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm">
+          <thead>
+            <tr className="border-b border-gray-200">
+              <th className="py-2 px-4 text-left">#</th>
+              <th className="py-2 px-4 text-left">User name</th>
+              <th className="py-2 px-4 text-left max-sm:hidden">Job Title</th>
+              <th className="py-2 px-4 text-left max-sm:hidden">Location</th>
+              <th className="py-2 px-4 text-left">Resume</th>
+              <th className="py-2 px-4 text-left">Action</th>
+            </tr>
+          </thead>
 
-export default ViewApplications
+          <tbody>
+            {viewApplicationsPageData.map((applicant, index) => (
+              <tr key={index}>
+                <td className="py-2 px-4 border-b border-gray-200 text-center">
+                  {index + 1}
+                </td>
+
+                <td className="py-2 px-4 border-b border-gray-200">
+                  <div className="flex items-center">
+                    <img
+                      className="w-10 h-10 rounded-full mr-3 max-sm:hidden"
+                      src={applicant.imgSrc}
+                      alt=""
+                    />
+                    <span>{applicant.name}</span>
+                  </div>
+                </td>
+
+                <td className="py-2 px-4 border-b border-gray-200 max-sm:hidden">
+                  {applicant.jobTitle}
+                </td>
+
+                <td className="py-2 px-4 border-b border-gray-200 max-sm:hidden">
+                  {applicant.location}
+                </td>
+
+                <td className="py-2 px-4 border-b border-gray-200">
+                  <a
+                    href=""
+                    target="_blank"
+                    className="bg-blue-50 text-blue-400 py-1 px-2 rounded inline-flex gap-2 items-center"
+                  >
+                    Resume
+                    <img src={assets.resume_download_icon} alt="" />
+                  </a>
+                </td>
+
+                <td className="py-2 px-4 border-b border-gray-200">
+                  <div className="relative inline-block group">
+                    <button type="button" className="text-gray-500 px-2 py-1">
+                      ...
+                    </button>
+
+                    {/* Dropdown */}
+                    <div
+                      className="
+                      hidden absolute right-0 top-full w-32 bg-white border border-gray-200 rounded shadow-lg z-50 group-hover:block
+                      "
+                    >
+                      <button
+                        type="button"
+                        className="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100"
+                      >
+                        Accept
+                      </button>
+
+                      <button
+                        type="button"
+                        className="block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100"
+                      >
+                        Reject
+                      </button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ViewApplications;
