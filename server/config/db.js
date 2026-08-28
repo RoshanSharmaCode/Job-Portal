a31792b7c86edd0b1b7d4e06575f498ed9fab560
Roshan <roshansharma9379@gmail.com>
2026-08-28 15:02:11 +0530

    Connected MongoDB database

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+import mongoose, { connect } from "mongoose";
+
+// Function to connect to MongoDB database
+const conectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Database connected"));
+
+  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+};
+
+export default conectDB;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
+import conectDB from "./config/db.js";
 
 // Initialize Express
 const app = express();
+
+// Connect to database
+
+await conectDB();
 
 // Middlewares
 app.use(cors());
