b7c1901ca845d570132df36206999140caf0739b
Roshan <roshansharma9379@gmail.com>
2026-08-31 14:58:52 +0530

    Created User model schema and webhooks controller

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-import "./config/instrument.js"
+import "./config/instrument.js";
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import conectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from "./controllers/webhooks.js";
 
 // Initialize Express
 const app = express();
@@ -17,10 +18,12 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.send('API Working'));
+app.get("/", (req, res) => res.send("API Working"));
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+
+app.post("/webhooks", clerkWebhooks);
 
 // Port
 const PORT = process.env.PORT || 5000;
@@ -28,6 +31,5 @@ const PORT = process.env.PORT || 5000;
 Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
-
