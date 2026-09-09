05709dc15ae4d71171e1eab1f4a7e22a0b3b7475
Roshan <roshansharma9379@gmail.com>
2026-09-09 14:32:14 +0530

    Fixed jobCard to show only job description

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -19,10 +19,9 @@ const JobCard = ({ job }) => {
           {job.level}
         </span>
       </div>
-      <p
-        className="text-gray-500 text-sm mt-4"
-        dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}
-      />
+      <p className="text-gray-500 text-sm mt-4 line-clamp-3">
+        {job.description.replace(/<[^>]*>/g, "").slice(0, 150)}
+      </p>
       <div className="mt-4 flex gap-4 text-sm">
         <button
           onClick={() => {
